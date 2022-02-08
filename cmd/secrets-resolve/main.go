8efa85fccf60570ce2bca817f7e34cf2dbf3809d
trif0lium <gh@fleeingsunlight.dev>
2022-02-08 20:51:55 +0700

    feat(cmd/secrets-resolve): os.Environ()

--- cmd/secrets-resolve/main.go
+++ cmd/secrets-resolve/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		envKey := pair[0]
+		envValue := pair[1]
+	}
+}
