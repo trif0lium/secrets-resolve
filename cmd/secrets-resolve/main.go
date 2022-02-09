b9066c548118e4c305b010a48fd6d4110407a390
trif0lium <gh@fleeingsunlight.dev>
2022-02-09 11:36:43 +0700

    feat(cmd/secrets-resolve): gcpInit.Do()

--- cmd/secrets-resolve/main.go
+++ cmd/secrets-resolve/main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"context"
+	"log"
 	"os"
 	"strings"
+	"sync"
+
+	"gocloud.dev/gcp"
+	"gocloud.dev/runtimevar/gcpsecretmanager"
+	"google.golang.org/genproto/googleapis/api"
 )
 
 func main() {
+	ctx := context.Background()
+	var gcpInit sync.Once
+	var gcpSecretManagerClient *api.Client
+	var gcpSecretManagerClientCleanFunc func()
+
 	for _, e := range os.Environ() {
 		pair := strings.SplitN(e, "=", 2)
 		envKey := pair[0]
-		envValue := pair[1]
+		envValue := strings.TrimSpace(pair[1])
+		if strings.HasPrefix(envValue, "gcpSecretManager://") {
+			parts := strings.Split(strings.TrimPrefix(envValue, "gcpSecretManager://"), "/")
+			if len(parts) != 2 {
+				log.Printf("invalid reference format: %s", envValue)
+				continue
+			}
+			gcpInit.Do(func() {
+				creds, err := gcp.DefaultCredentials(ctx)
+				if err != nil {
+					panic(err)
+				}
+				gcpSecretManagerClient, gcpSecretManagerClientCleanFunc, err := gcpsecretmanager.Dial(ctx, gcp.TokenSource(creds))
+				if err != nil {
+					panic(err)
+				}
+			})
+		}
+	}
+
+	if gcpSecretManagerClient != nil {
+		gcpSecretManagerClientCleanFunc()
 	}
 }
